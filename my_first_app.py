01ada7d98c042fae71861f84f688c5448e4acd34
kalopez0621 <145211794+kalopez0621@users.noreply.github.com>
2024-11-06 21:31:51 -0500

    Update my_first_app.py

--- my_first_app.py
+++ my_first_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+# Title
+st.title('My First Streamlit App')
+
+# Select Box
+options = ["Option 1", "Option 2", "Option 3"]
+selection = st.selectbox("Choose an option:", options)
+st.write("You selected:", selection)
+
+# Button
+if st.button("Click Me"):
+    st.write("Button was clicked!")
 
